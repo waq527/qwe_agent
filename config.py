4ad643d45acc74a8f676e1e3410977220a905aed
qwe <2982448269@qq.com>
2026-01-07 10:14:58 +0800

    qwe agent

--- config.py
+++ config.py
@@ -14,11 +14,11 @@ DB_CONFIG = {
         "default": {
             "engine": "tortoise.backends.asyncpg",
             "credentials": {
-                "host": os.getenv("DB_HOST", "localhost"),
-                "port": os.getenv("DB_PORT", 5432),
-                "user": os.getenv("DB_USER", "qwe"),
-                "password": os.getenv("DB_PASSWORD", "qwe123456"),
-                "database": os.getenv("DB_NAME", "UG"),
+                "host": os.getenv("DB_HOST"),
+                "port": os.getenv("DB_PORT"),
+                "user": os.getenv("DB_USER"),
+                "password": os.getenv("DB_PASSWORD"),
+                "database": os.getenv("DB_NAME"),
             }
         }
     },
